d8d302af29a2c0289d5c77d82e9f6eb3b88b1320
Phillip Haberman <pjhab2020@gmail.com>
2025-11-09 22:13:40 +0800

    rm revenue from top products

--- transformations/top_products_card.py
+++ transformations/top_products_card.py
@@ -2,10 +2,10 @@ import pandas as pd
 
 def top_products_card(df: pd.DataFrame) -> pd.DataFrame:
 
-    top_products = df.groupby(['category', 'product']).agg({'quantity': 'sum', 'total': 'sum'})
+    top_products = df.groupby(['category', 'product']).agg({'quantity': 'sum'})
 
     top_products_sorted = top_products.sort_values(by='quantity', ascending=False).reset_index()
 
-    top_products_sorted.columns = ['Category', 'Product', 'Quantity', 'Total']
+    top_products_sorted.columns = ['Category', 'Product', 'Quantity']
 
     return top_products_sorted.head(5)
